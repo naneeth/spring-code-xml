48c96d1117a3211f1fc1a6b233989e13c7a9bcd3
naneeth <naneethc003@gmail.com>
2019-01-15 23:54:38 +0530

    final

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -15,18 +15,23 @@ public class Main {
     public static void main(String[] args) {
 
 
-        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie movie = (Movie) beanFactory.getBean("movie");
-        System.out.println(movie.getActor());
-
-        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
-        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-        Movie movie1 = (Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie");
-        System.out.println(movie1.getActor());
+//        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+//        Movie movie = (Movie) beanFactory.getBean("movie");
+//        System.out.println(movie.getActor());
+//
+//        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
+//        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
+//        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
+//        Movie movie1 = (Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie");
+//        System.out.println(movie1.getActor());
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie2 = (Movie) applicationContext.getBean("movie");
+        Movie movie1 = (Movie) applicationContext.getBean("movie1");
+        System.out.println(movie1.getActor());
+        ApplicationContext applicationContext2 = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie2 = (Movie) applicationContext2.getBean("movie1");
         System.out.println(movie2.getActor());
+        System.out.println(movie1==movie2);
+
     }
 }
