34df4f6caff642b213c593914f11db6b2d7060c5
naneeth <naneethc003@gmail.com>
2019-01-16 10:02:07 +0530

    finall

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,29 +1,11 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 public class Main {
     public static void main(String[] args) {
-
-
-//        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-//        Movie movie = (Movie) beanFactory.getBean("movie");
-//        System.out.println(movie.getActor());
-//
-//        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
-//        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-//        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-//        Movie movie1 = (Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie");
-//        System.out.println(movie1.getActor());
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
         Movie movie1 = (Movie) applicationContext.getBean("movie1");
@@ -32,6 +14,5 @@ public class Main {
         Movie movie2 = (Movie) applicationContext2.getBean("movie1");
         System.out.println(movie2.getActor());
         System.out.println(movie1==movie2);
-
     }
 }
